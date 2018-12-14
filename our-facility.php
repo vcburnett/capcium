29620650e60ad9d80cde9e781bff4f8d00baf5e3
vcburnett <victorburnett@Victors-iMac-2.local>
2018-12-14 00:55:56 -0500

    Multiple changes

---
+++ our-facility.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1" name="viewport" />
+<title>Capcium</title>
+
+<link rel="shortcut icon" href="assets/images/favicon.png"/>
+
+<link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700|Raleway:600" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+	<?php include_once("modules/header.php"); ?>
+
+	<div id="wrapper">
+		<!-- HERO -->
+		<section class="inner-header" id="page-our-facility">
+			<div class="inner-header-content">
+				<h1>The future is now</h1>
+				<h4>Our Facility</h4>
+			</div>
+			<div id="hero-fluid-bottom"><img src="assets/images/hero-internal-bottom.svg"></div>
+		</section>
+
+		<a href="#sec-first-content" id="btn-down-internal" class="scroll"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 34 34" style="enable-background:new 0 0 34 34;" xml:space="preserve"><path d="M17,29.56c-0.54,0-1.08-0.21-1.49-0.62L0.62,14.05c-0.83-0.82-0.83-2.16,0-2.99c0.82-0.83,2.16-0.83,2.99,0L17,24.46l13.39-13.39c0.83-0.83,2.16-0.83,2.99,0c0.83,0.82,0.83,2.16,0,2.99L18.49,28.94C18.08,29.35,17.54,29.56,17,29.56z"/></svg></a>
+
+		<!-- Content -->
+		<section class="content" id="sec-first-content">
+			<div class="content-wrapper">
+				<div class="main-text-wrap">
+					<p>Capcium has developed a two-phase commercial strategy to best serve our licensed cannabis producer partners as well as our nutraceutical and pharmaceutical customers.</p>
+				</div>
+
+				<div class="full-wrap">
+					<h5>Phase 1</h5>
+					<div class="facility-wrap-small">
+						<div class="facility-pic-small">
+							<img src="assets/images/-placeholders/facility-example.png" alt="Capcium Inc. facility">
+						</div>
+						<div class="facility-text-small">
+							<h2>On-Site Nutraceutical Production</h2>
+							<p>Capcium received its site license from Health Canada to manufacture softgel capsules for the nutraceutical industry in 2015. Since then, we have been producing sofgels for our customers in our cGMP compliant St. Laurent, Quebec facility.</p>
+						</div>
+					</div>
+					<div class="facility-wrap">
+						<div class="facility-pic">
+							<img src="assets/images/-placeholders/facility-example.png" alt="Capcium Inc. facility">
+						</div>
+						<div class="facility-text">
+							<h2>Co-Location of Encapsulation Equipment for Cannabis Product Production</h2>
+							<p>Until our new, state-of-the-art Pointe-Claire facility is fully operational (currently expected to end 2019), Capcium will produce softgel capsules for Licensed Producers at a premise licensed under the Access to Cannabis for Medical Purposes Regulations (ACMPR). To date, Capcium has one fully operational encapsulation line at Aurora’s Montreal-based facility (“Aurora Vie”) and is in the process of completing the installation of additional encapsulation lines at other sites licensed under the ACMPR to service further cannabis customers’ needs.</p>
+						</div>
+					</div>
+				</div>
+				<div class="full-wrap">
+					<h5>Phase 2</h5>
+					<div class="facility-wrap">
+						<div class="facility-pic-alt">
+							<img src="assets/images/-placeholders/facility-example.png" alt="Capcium Inc. facility">
+						</div>
+						<div class="facility-text-alt">
+							<h2>All Production Under One Roof</h2>
+							<p>Phase II of Capcium’s commercial plan will be to produce all cannabis, pharmaceutical and nutraceutical products at our new facility in Pointe-Claire, Quebec. Construction of the Pointe-Claire Facility is well advanced and is expected to be completed in late 2019.</p>
+							<p>The new Facility will be the first of its kind, state-of-the-art facility, designed specifically for the development and manufacture of cannabis products. The Facility will be pharmaceutical grade and compliant with all major cGMP pharmaceutical standards, including Health Canada, FDA and EMEA.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+	</div>
+
+	<?php include_once("modules/footer.php"); ?>
+
+</body>
+</html>
---
+++ what-we-do.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1" name="viewport" />
+<title>Capcium</title>
+
+<link rel="shortcut icon" href="assets/images/favicon.png"/>
+
+<link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700|Raleway:600" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+	<?php include_once("modules/header.php"); ?>
+
+	<div id="wrapper">
+		<!-- HERO -->
+		<section class="inner-header" id="page-what-we-do">
+			<div class="inner-header-content">
+				<h1>Helping You Bring Your Products to Life</h1>
+				<h4>What We Do</h4>
+			</div>
+			<div id="hero-fluid-bottom"><img src="assets/images/hero-internal-bottom.svg"></div>
+		</section>
+
+		<a href="#sec-first-content" id="btn-down-internal" class="scroll"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 34 34" style="enable-background:new 0 0 34 34;" xml:space="preserve"><path d="M17,29.56c-0.54,0-1.08-0.21-1.49-0.62L0.62,14.05c-0.83-0.82-0.83-2.16,0-2.99c0.82-0.83,2.16-0.83,2.99,0L17,24.46l13.39-13.39c0.83-0.83,2.16-0.83,2.99,0c0.83,0.82,0.83,2.16,0,2.99L18.49,28.94C18.08,29.35,17.54,29.56,17,29.56z"/></svg></a>
+
+		<!-- Content -->
+		<section class="content" id="sec-first-content">
+			<div class="content-wrapper">
+				<div class="main-text-wrap">
+					<p>Our core focus is softgel encapsulation of cannabis-derived oils. A delicate combination of both science and art, softgel encapsulation is a tricky task to perfect which creates an obstacle for most licensed cannabis producers as they don’t have the know-how or elaborate infrastructure required. Our unique combination of strategic and hands-on expertise will help you to deliver high-quality products to your consumers.</p>
+					<p>Capcium is an innovative, pharmaceutical grade and fully licensed value-add service provider to its customers. We consistently deliver high-quality products that you can trust to put your name to.</p>
+					<div class="text-division"></div>
+					<h2>Why Choose Capcium</h2>
+					<p>When you need to trust another company with your precious product, choose the one that is laser-focused on your success.</p>
+				</div>
+				<div class="full-wrap">
+					<div class="quarter-element">
+						<div class="image-wrap">
+							<img src="assets/images/-placeholders/img-example-2.png" alt="Partner Example">
+						</div>
+						<div class="text text-left">
+							<h3>Experience</h3>
+							<ul class="internal-content-list">
+								<li>Leveraging 20+ years of encapsulation experience</li>
+								<li>Industry-first cannabis softgels</li>
+								<li>Pharmaceutical expertise</li>
+							</ul>
+						</div>
+					</div>
+					<div class="quarter-element">
+						<div class="image-wrap">
+							<img src="assets/images/-placeholders/img-example-2.png" alt="Partner Example">
+						</div>
+						<div class="text text-left">
+							<h3>We have the team</h3>
+							<ul class="internal-content-list">
+								<li>Dedicated management team</li>
+								<li>Highly trained and specialized production team</li>
+							</ul>
+						</div>
+					</div>
+					<div class="quarter-element">
+						<div class="image-wrap">
+							<img src="assets/images/-placeholders/img-example-2.png" alt="Partner Example">
+						</div>
+						<div class="text text-left">
+							<h3>We have the infrastructure</h3>
+							<ul class="internal-content-list">
+								<li>State-of-the-art facility</li>
+								<li>Precision encapsulation equipment</li>
+								<li>Innovative delivery methods</li>
+							</ul>
+						</div>
+					</div>
+					<div class="quarter-element">
+						<div class="image-wrap">
+							<img src="assets/images/-placeholders/img-example-2.png" alt="Partner Example">
+						</div>
+						<div class="text text-left">
+							<h3>We give you peace of mind</h3>
+							<ul class="internal-content-list">
+								<li>We take the worries of this pivotal step of the production process off of your hands</li>
+								<li>Let you focus on what you do best: extraction, cultivation, branding, etc.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="main-text-wrap">
+					<h2>Our Services</h2>
+					<p>We bring our unique skill set to every project. Our extensive experience, expertise, and commitment to the best customer service ensures that your product is in the best hands.</p>
+				</div>
+				<div class="full-wrap">
+					<div class="services-tabs">
+
+					</div>
+					<div class="services-wrap">
+						<div class="service-content">
+							<div class="service-content-image">
+								<img src="assets/images/-placeholders/img-example-square.png" alt="Cannabis">
+							</div>
+							<div class="service-content-text">
+								<h2>Quality & Compliance</h2>
+								<p>Quality assurance is the cornerstone of Capcium's manufacturing practices. We are committed to ensuring and maintaining the highest standards of regulatory compliance while providing high-quality products to our customers. A comprehensive quality assurance program along with strict product regulations are in place to ensure that we meet these high-quality standards.</p>
+								<p>An internal audit program monitors compliance with applicable regulations, standards, and internal policies. We test and analyze all raw materials and finished products to determine potency, purity and quality. A Certificate of Analysis accompanies all manufactured lots and includes actual results and references to product specifications.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="main-text-wrap">
+					<h2>Why Softgels?</h2>
+					<p>With so many desirable characteristics and advantages over other dosing formats, Softgels are a popular choice for pharmaceutical, nutraceutical and cannabis brands as well as their end users.</p>
+					<a href="softgels.php" class="cta-3">Get to know our team</a>
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<?php include_once("modules/footer.php"); ?>
+
+</body>
+</html>
